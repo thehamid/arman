1bcd286d73b0c31b8d7284bb7df5ca4479f8e6c7
thehamid <info@thehamid.ir>
2021-12-05 14:59:55 +0330

    fixed style page

--- page.php
+++ page.php
@@ -8,14 +8,14 @@
 <?php get_header(); ?>
 <div class="page" role="main">
      <div class="container">
-        <?php if(function_exists('web_breadcrumb')) web_breadcrumb(); ?>
+<!--        --><?php //if(function_exists('web_breadcrumb')) web_breadcrumb(); ?>
 
         <?php while (have_posts()) : the_post(); ?>
 
-            <div class="page-content">
-                <h1><?php the_title(); ?></h1>
-                <p> <?php the_content(); ?></p>
 
+            <h1><?php the_title(); ?></h1>
+            <div class="page-content">
+                 <?php the_content(); ?>
             </div>
 
         <?php if(comments_open() || get_comments_number()):?>

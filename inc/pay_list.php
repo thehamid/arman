6386b8106b9d79ed38cfd08c2cfa6b0c62b19ee0
thehamid <info@thehamid.ir>
2021-12-26 15:04:58 +0330

    table pay list new design

--- inc/pay_list.php
+++ inc/pay_list.php
@@ -1,49 +1,26 @@
 <?php
 
-global $wpdb;
-$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}projects_donors");
+include "donors_list.php";
 
-?>
+    //Create an instance of our package class...
+    $testListTable = new TT_Example_List_Table();
+    //Fetch, prepare, sort, and filter our data...
+    $testListTable->prepare_items();
 
+    ?>
     <div class="wrap">
+
+        <div id="icon-users" class="icon32"><br/></div>
         <h2> لیست پرداخت‌ها </h2>
 
-        <table class="widefat" >
-            <thead>
-            <tr>
-                <th>ردیف</th>
-                <th>نام</th>
-                <th>تلفن</th>
-                <th>مبلغ</th>
-                <th>پروژه</th>
-                <th>وضعیت</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($results as $result): ?>
-                <tr>
-                    <th><?php echo $result->id?></th>
-                    <th><?php echo $result->name?></th>
-                    <th><?php echo $result->phone?></th>
-                    <th><?php echo $result->value?></th>
-                    <th><?php echo get_the_title($result->project_id); ?></th>
-                    <th>
-                        <?php
-                        switch($result->status)
-                        {
-                            case '-1' : echo "خطا در پرداخت"; break;
-                            case '-2' : echo "در انتظار پرداخت"; break;
-                            case '0' : echo "خطا یا انصراف در پرداخت"; break;
-                            case '1' : echo "پرداخت موفق"; break;
-                            default : echo "خطای نامشخص!";
-                        }
 
-                        ?>
-                    </th>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-
+        <!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
+        <form id="movies-filter" method="get">
+            <!-- For plugins, we also need to ensure that the form posts back to our current page -->
+            <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+            <!-- Now we can render the completed list table -->
+            <?php $testListTable->display() ?>
+        </form>
 
     </div>
+    <?php

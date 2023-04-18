76cfd14702dd0abe86425b5b462f39503170fd97
thehamid <info@thehamid.ir>
2023-04-18 14:23:01 +0430

    fix style crowd section index

--- template/index-events.php
+++ template/index-events.php
@@ -11,15 +11,15 @@
             <div class="col-sm-12 col-md-6 col-lg-4 ">
                 <div class="items-events">
                     <a href="<?php echo home_url(); ?>/memories" class="e-item wow fadeInRight">
-                        <i class="fad fa-home-heart"></i>
+                        <i class="fad fa-home-heart fa-beat"></i>
                         <h4>خاطرات خانه</h4>
                     </a>
                     <a href="<?php echo home_url(); ?>/events" class="e-item wow fadeInLeft">
-                        <i class="fad fa-calendar-alt"></i>
+                        <i class="fad fa-calendar-alt fa-shake"></i>
                         <h4>رویداد‌های خانه</h4>
                     </a>
                     <a href="<?php echo home_url(); ?>/successes" class="e-item wow fadeInRight">
-                        <i class="fad fa-star"></i>
+                        <i class="fad fa-star fa-spin"></i>
                         <h4>موفقیت‌های خانه</h4>
                     </a>
 
